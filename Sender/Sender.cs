6808bb60037d7d646d504a3201abcde452ee6c53
Vlad Mardari <mardari1998@gmail.com>
2021-10-07 20:33:20 +0300

    added client class

---
+++ Sender/Sender.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using Internal;
+using System;
+using Common;
+using Grpc.Net.Client;
+using GrpcAgent;
+
+namespace Sender
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress(EndpointConstants.BrokerAddress);
+            var client = new Publisher.PublisherClient(channel);
+
+            while(true)
+            {
+                Console.Write("Enter the topic: ");
+                var topic = Console.ReadLine.ToLower();
+
+                console.Write("Enter content: ");
+                var content = ConstraintCollection.ReadLine();
+
+                var request = new PublishRequest() {Topic = topic, Content = content};
+
+                try 
+                {
+                    var reply = await client.PublishMessageAsync(request);
+                    Console.WriteLine(reply.IsSuccess)
+                }
+                catch(Exceotion e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
